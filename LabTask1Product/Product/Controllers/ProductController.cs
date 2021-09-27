d42b46d8c2b7ff4c7bc3c957c7e7b75c90bef119
Avijit Paul <er.avijit18@gmail.com>
2021-09-27 14:05:44 +0600

    Lab task 1 : Product Input and View

---
+++ LabTask1Product/Product/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Product.Models;
+using Product.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Database db = new Database();
+            var products = db.Products.GetAll();
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ProductStructure p)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+    }
+}
